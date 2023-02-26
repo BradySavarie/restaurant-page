20d16089d25d2bef4c9638d78ff8db5a994106fb
Brady <brady.savarie@hotmail.com>
2023-02-26 01:32:09 -0500

    Reconfigure tailwind for use with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,30 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    mode: 'development',
     devtool: 'inline-source-map',
+    entry: './src/index.js',
     output: {
-        filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist'),
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                include: path.resolve(__dirname, 'src'),
+                use: ['style-loader', 'css-loader', 'postcss-loader'],
+            },
+        ],
     },
 };
