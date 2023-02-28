eb3201277eb27d5db4b61eb3aac3dbc0c36b78c5
Brady <brady.savarie@hotmail.com>
2023-02-28 00:31:33 -0500

    Style home page elements

--- src/home.js
+++ src/home.js
@@ -9,23 +9,22 @@ homePage.classList.add(
     'rounded-3xl',
     'flex',
     'flex-col',
-    'gap-12',
+    'justify-between',
     'p-7',
     'md:px-20',
-    'md:py-5',
-    'relative'
+    'md:py-5'
 );
 
 homePage.innerHTML = `
 <!-- HEADER -->
 <nav class='h-16 sm:h-20 flex justify-between items-center'>
     <img src='../src/img/logo-ipsum.svg' class='h-16 sm:h-20'>
-    <ul class='sm:flex gap-8 outline justify-between font-Poppins font-light text-2xl hidden'>
+    <ul class='sm:flex gap-8 justify-between font-Poppins font-light text-2xl hidden'>
         <li><button>Home</button></li>
         <li><button>Menu</button></li>
         <li><button>Contact</button></li>
     </ul>
-    <button><img src='../src/img/menu-icon.svg' class='h-7 sm:hidden'></button>
+    <button class='sm:hidden'><img src='../src/img/menu-icon.svg' class='h-7'></button>
     <button class='sm:flex text-lg font-bold border-2 border-deep-red px-4 hidden'>VIEW ORDER</button>
 </nav>
 
@@ -36,17 +35,16 @@ homePage.innerHTML = `
         <p class='text-md font-Poppins font-normal'>Try our new enhanced beef jerky to help build muscle on-the-go</p>
         <button class='hidden'></button>
     </div>
-    <img src='../src/img/beef-jerky.png' class='md:max-w-[50%] h-auto mt-6 mb-10'>
+    <img src='../src/img/beef-jerky.png' class='md:max-w-[50%] h-auto my-6'>
     <button class='sm:hidden text-lg font-bold text-white bg-deep-red w-[max-content] px-8 py-2 font-Poppins tracking-wide shadow-md self-center'>READ MORE</button>
 </div>
 
 <!-- FOOTER -->
-<div class='absolute bottom-8 flex items-center'>
-    <ul class='flex font-Poppins text-md'>
-        <li class='flex gap-2'><img src='../src/img/facebook-icon.svg'>FACEBOOK</li>
-        <li class='flex gap-2'><img src='../src/img/instagram-icon.svg'>INSTAGRAM</li>
-    </ul>
-    <p class='hidden'>Find us on Skip The Dishes!</p>
+<div class='flex justify-between items-center'>
+    <div class='flex gap-2'><img src='../src/img/facebook-icon.svg'><span>FACEBOOK</span></div>
+    <div class='flex gap-2'><img src='../src/img/instagram-icon.svg'><span>INSTAGRAM</span></div>
+    <div class='h-[5%] hidden md:flex basis-1/2 lg:basis-1/4 bg-deep-red'></div>
+    <p class='hidden lg:flex font-Poppins font-normal'>We're on Skip The Dishes!</p>
 </div>`;
 
 content.appendChild(homePage);
