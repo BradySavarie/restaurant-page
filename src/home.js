16de893a283788c2e901c6d32d0dd9a026ff7ed6
Brady <brady.savarie@hotmail.com>
2023-02-28 23:31:41 -0500

    Style main breakpoints

--- src/home.js
+++ src/home.js
@@ -22,7 +22,7 @@ homePage.innerHTML = `
 <!-- HEADER -->
 <nav class='h-16 flex w-full justify-between items-center'>
     <img src='../src/img/logo-ipsum.svg' class='h-16 md:h-20'>
-    <ul class='sm:flex gap-6 justify-between font-Poppins font-light text-xl lg:text-2xl hidden'>
+    <ul class='sm:flex gap-6 md:gap-10 justify-between font-Poppins font-light text-xl lg:text-2xl hidden'>
         <li><button>Home</button></li>
         <li><button>Menu</button></li>
         <li><button>Contact</button></li>
@@ -32,14 +32,14 @@ homePage.innerHTML = `
 </nav>
 
 <!-- MAIN -->
-<div class='flex flex-col md:flex-row justify-between items-center max-w-[20rem]'>
+<div class='flex flex-col md:flex-row justify-between items-center max-w-[20rem] md:max-w-full lg:max-w-full md:px-10'>
     <div class='flex flex-col gap-2 relative'>
-        <p class='text-sm font-semibold -rotate-6 text-deep-red mb-1 absolute -top-5'>NEW!</p>
-        <h1 class='text-4xl font-bold font-Poppins leading-10'>PERFORMANCE<br>PROTEIN</h1>
-        <p class='text-md font-Poppins font-normal'>Try our new and enhanced beef jerky to build muscle on-the-go</p>
-        <button class='hidden'></button>
+        <p class='text-sm md:text-md lg:text-lg font-semibold -rotate-6 text-deep-red mb-1 absolute -top-5'>NEW!</p>
+        <h1 class='text-4xl md:text5xl lg:text-6xl font-bold font-Poppins leading-10'>PERFORMANCE<br>PROTEIN</h1>
+        <p class='text-md md:text-lg lg:text-xl font-Poppins font-normal'>Try our new and enhanced beef jerky to build muscle on-the-go</p>
+        <button class='hidden md:flex text-lg font-bold text-white bg-deep-red w-[max-content] px-8 py-2 font-Poppins tracking-wide shadow-md mt-4'>READ MORE</button>
     </div>
-    <img src='../src/img/beef-jerky.png' class='md:max-w-[50%] max-w-[90%] h-auto my-6'>
+    <img src='../src/img/beef-jerky.png' class='md:max-w-[50%] max-w-[90%] lg:max-w-[40%] h-auto my-6'>
     <button class='md:hidden text-lg font-bold text-white bg-deep-red w-[max-content] px-8 py-2 font-Poppins tracking-wide shadow-md self-center'>READ MORE</button>
 </div>
 
@@ -48,7 +48,7 @@ homePage.innerHTML = `
     <div class='flex gap-2'><img src='../src/img/facebook-icon.svg'><span>FACEBOOK</span></div>
     <div class='flex gap-2'><img src='../src/img/instagram-icon.svg'><span>INSTAGRAM</span></div>
     <div class='h-[5%] hidden sm:flex basis-1/2 lg:basis-1/4 bg-deep-red'></div>
-    <p class='hidden lg:flex font-Poppins font-normal'>We're on Skip The Dishes!</p>
+    <p class='hidden lg:flex font-Poppins font-normal'>FIND US ON SKIP THE DISHES!</p>
 </div>`;
 
 content.appendChild(homePage);
