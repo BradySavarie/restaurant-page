72db326f855772e287608cb20eaf533675675974
Brady <brady.savarie@hotmail.com>
2023-03-01 19:32:07 -0500

    Implement tabbed browsing

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import './styles.css';
-import './home';
+import renderHome from './home';
+import renderMenu from './menu';
+import renderContact from './contact';
 
-console.log('Index Test Successful');
+renderHome();
+
+window.addEventListener('click', (e) => {
+    if (e.target.id === 'menu') {
+        renderMenu();
+    } else if (e.target.id === 'contact' || e.target.id === 'placeOrderBtn') {
+        renderContact();
+    } else if (e.target.id === 'home') {
+        renderHome();
+    }
+});
---
+++ src/menu.js
@@ -0,0 +1,36 @@
+export default function renderMenu() {
+    const content = document.getElementById('content');
+    content.innerHTML = '';
+    content.classList.add('h-screen', 'w-screen', 'p-7');
+    const homePage = document.createElement('div');
+    homePage.classList.add(
+        'h-full',
+        'w-full',
+        'border-4',
+        'border-dark-gray',
+        'rounded-3xl',
+        'flex',
+        'flex-col',
+        'justify-between',
+        'px-7',
+        'py-5',
+        'md:px-9',
+        'md:py-8',
+        'gap-3',
+        'items-center'
+    );
+
+    homePage.innerHTML = `<!-- HEADER -->
+<nav class='h-16 flex w-full justify-between items-center'>
+    <img src='../src/img/logo-ipsum.svg' class='h-16 md:h-20'>
+    <ul class='sm:flex gap-6 md:gap-10 justify-between font-Poppins font-light text-xl lg:text-2xl hidden'>
+        <li><button id='home' class='hover:underline decoration-deep-red underline-offset-4'>Home</button></li>
+        <li><button id='menu' class='hover:underline decoration-deep-red underline-offset-4 font-semibold'>Menu</button></li>
+        <li><button id='contact' class='hover:underline decoration-deep-red underline-offset-4'>Contact</button></li>
+    </ul>
+    <button class='sm:hidden'><img src='../src/img/menu-icon.svg' class='h-7'></button>
+    <button id='placeOrderBtn' class='sm:flex text-sm font-bold outline outline-2 outline-deep-red px-4 py-1 hidden hover:text-white hover:bg-deep-red'>PLACE ORDER</button>
+</nav>`;
+
+    content.appendChild(homePage);
+}
