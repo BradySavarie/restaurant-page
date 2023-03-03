98e2b1c7d55d5d6db91dca99ab439e714d145bd1
Brady <brady.savarie@hotmail.com>
2023-03-02 22:53:12 -0500

    Replace hero image & prep each page for styling

--- src/contact.js
+++ src/contact.js
@@ -1,10 +1,11 @@
 export default function renderContact() {
     const content = document.getElementById('content');
     content.innerHTML = '';
-    content.classList.add('h-screen', 'w-screen', 'p-7');
+    content.classList.add('h-screen', 'w-screen', 'p-5', 'overflow-auto');
+
     const homePage = document.createElement('div');
     homePage.classList.add(
-        'h-full',
+        'min-h-full',
         'w-full',
         'border-4',
         'border-dark-gray',
@@ -17,10 +18,13 @@ export default function renderContact() {
         'md:px-9',
         'md:py-8',
         'gap-3',
-        'items-center'
+        'items-center',
+        'shadow-lg',
+        'bg-white'
     );
 
-    homePage.innerHTML = `<!-- HEADER -->
+    homePage.innerHTML = `
+<!-- HEADER -->
 <nav class='h-16 flex w-full justify-between items-center'>
     <img src='../src/img/logo-ipsum.svg' class='h-16 md:h-20'>
     <ul class='sm:flex gap-6 md:gap-10 justify-between font-Poppins font-light text-xl lg:text-2xl hidden'>
@@ -30,7 +34,19 @@ export default function renderContact() {
     </ul>
     <button class='sm:hidden'><img src='../src/img/menu-icon.svg' class='h-7'></button>
     <button id='placeOrderBtn' class='sm:flex text-sm font-bold outline outline-2 outline-deep-red px-4 py-1 hidden hover:text-white hover:bg-deep-red'>PLACE ORDER</button>
-</nav>`;
+</nav>
+
+<!-- MAIN -->
+<div class=''>
+</div>
+
+<!-- FOOTER -->
+<div class='flex w-full justify-around sm:justify-between items-center'>
+    <div class='flex gap-2'><img src='../src/img/facebook-icon.svg'><span>FACEBOOK</span></div>
+    <div class='flex gap-2'><img src='../src/img/instagram-icon.svg'><span>INSTAGRAM</span></div>
+    <div class='h-[.1rem] hidden sm:flex basis-1/2 lg:basis-1/4 bg-deep-red'></div>
+    <p class='hidden lg:flex font-Poppins font-normal'>FIND US ON SKIP THE DISHES!</p>
+</div>`;
 
     content.appendChild(homePage);
 }
--- src/home.js
+++ src/home.js
@@ -1,7 +1,8 @@
 export default function renderHome() {
     const content = document.getElementById('content');
     content.innerHTML = '';
-    content.classList.add('h-screen', 'w-screen', 'p-7', 'overflow-auto');
+    content.classList.add('h-screen', 'w-screen', 'p-5', 'overflow-auto');
+
     const homePage = document.createElement('div');
     homePage.classList.add(
         'min-h-full',
@@ -17,7 +18,9 @@ export default function renderHome() {
         'md:px-9',
         'md:py-8',
         'gap-3',
-        'items-center'
+        'items-center',
+        'shadow-lg',
+        'bg-white'
     );
 
     homePage.innerHTML = `
@@ -34,14 +37,14 @@ export default function renderHome() {
 </nav>
 
 <!-- MAIN -->
-<div class='flex justify-center flex-col md:flex-row items-center max-w-[70%] md:max-w-full lg:max-w-full md:px-10 lg:px-14'>
+<div class='flex justify-center flex-col lg:flex-row items-center max-w-[70%] md:max-w-full lg:max-w-full md:px-10 lg:px-14 mb-5 mt-5 md:mb-0 md:mt-0'>
     <div class='flex flex-col gap-2 relative'>
-        <p class='text-sm md:text-md lg:text-lg font-semibold -rotate-6 text-deep-red mb-1 absolute -top-5'>NEW!</p>
-        <h1 class='text-4xl md:text-5xl font-bold font-Poppins leading-10'>PERFORMANCE<br>PROTEIN</h1>
-        <p class='text-md md:text-lg lg:text-lg font-Poppins font-normal'>Try our new and enhanced beef jerky to build muscle on-the-go</p>
+        <p class='text-sm sm:text-md lg:text-lg font-semibold -rotate-6 text-deep-red mb-1 absolute -top-5 lg:-top-6'>NEW!</p>
+        <h1 class='text-4xl sm:text-5xl font-bold font-Poppins leading-10'>PERFORMANCE<br>PROTEIN</h1>
+        <p class='text-md sm:text-lg lg:text-lg font-Poppins font-normal'>Try our new and enhanced beef jerky to build muscle on-the-go</p>
         <button class='hidden md:flex text-lg font-bold text-white bg-deep-red w-[max-content] px-8 py-2 font-Poppins tracking-wide shadow-md mt-4 hover:outline hover:outline-2 hover:outline-deep-red hover:bg-white hover:text-dark-gray'>READ MORE</button>
     </div>
-    <img src='../src/img/beef-jerky.png' class='md:max-w-[50%] max-w-[90%] lg:max-w-[40%] h-auto my-6'>
+    <img src='../src/img/logo.svg' class='w-full sm:max-w-[70%] md:max-w-[35%] my-6'>
     <button class='md:hidden text-lg font-bold text-white bg-deep-red w-[max-content] px-8 py-2 font-Poppins tracking-wide shadow-md self-center'>READ MORE</button>
 </div>
 
