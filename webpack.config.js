34b9f05db51025026ee0e41f142baeb948038732
Brady <brady.savarie@hotmail.com>
2023-03-02 23:45:58 -0500

    Create contact form

--- webpack.config.js
+++ webpack.config.js
@@ -25,10 +25,6 @@ module.exports = {
                 include: path.resolve(__dirname, 'src'),
                 use: ['style-loader', 'css-loader', 'postcss-loader'],
             },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
         ],
     },
 };
